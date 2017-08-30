ff46e40f66f8584c1087f4378a11f95515c2ae76
Darcie Browning <darcie.browning@my.jcu.edu.au>
2017-08-30 23:26:45 +1000

    Php form layout for Add Student
    Created php file and form for add student, messed around on the server
    with these files.

--- addstudent.php
+++ addstudent.php
@@ -19,7 +19,26 @@
 </nav>
 
 <article>
- 
+
+<form method="post" action="">
+    <label>First Name</label>
+    <input type="text" name="first_name"/>
+    
+    <label>Last Name</label>
+    <input type="text" name="last_name"/>
+    <br>
+    
+    <label>Student Email</label>
+    <input type="text" name="stu_email"/>
+    <br>
+    
+    <label>Parent/Guardian Email</label>
+    <input type="text" name="par_email"/>
+    <br>
+
+<input type="submit" value="Add Employee">
+</form>
+    
 </article>
 
 
